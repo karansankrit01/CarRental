448f772ac6ee866ef13a6c98d9ffc141596f1e87
karansankrit01 <sankritkaran1@gmail.com>
2026-01-08 22:16:58 +0530

    worked on add new car section

--- src/pages/Owner/DashBoard.jsx
+++ src/pages/Owner/DashBoard.jsx
@@ -100,8 +100,8 @@ const DashBoard = () => {
         {/*monthly revenue chart*/}
         <div className="p-4 md:p-6 mb-6 border border-borderColor rounded-md w-full md:max-w-xs">
             <h1 className="text-lg font-medium">Monthly Revenue</h1>
-            <p>Revenue for current month</p>
-            <p>{currency}{data.monthlyRevenue}</p>
+            <p className="text-gray-500">Revenue for current month</p>
+            <p className="text-3xl mt-6 font-semibold text-primary">{currency}{data.monthlyRevenue}</p>
 
 
         </div>
