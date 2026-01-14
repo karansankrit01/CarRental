9e1d0e7a72a837d73c797392317138101484c757
karansankrit01 <sankritkaran1@gmail.com>
2026-01-15 01:10:37 +0530

    small added database(mongoose)

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+// Intialize Express App
+const app = express()
+
+// middleware
+app.use(cors());
+app.use(express.json());
+  
+app.get('/', (req,res)=> res.send("Server is running"))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+
+
